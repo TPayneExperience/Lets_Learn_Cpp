37f6a445459a440c3f23351616b48a42fc37665e
Trevor Payne <crashandexplode@hotmail.com>
2016-07-20 20:16:19 -0700

    WIP

---
+++ 01_Setup_First_Program/Debug_Exercises/Debug_01/Debug_01/main.cpp
@@ -0,0 +1,30 @@
+// SOLUTION AT BOTTOM
+
+#include <iostream>
+
+int main() {
+	std::cout << "Ham Sandwich\n"
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Line 6 missing semicolon
---
+++ 01_Setup_First_Program/Debug_Exercises/Debug_02/Debug_02/main.cpp
@@ -0,0 +1,30 @@
+// SOLUTION AT BOTTOM
+
+include <iostream>
+
+int main() {
+	std::cout << "Ham Sandwich\n";
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Line 3 missing # in front of include
